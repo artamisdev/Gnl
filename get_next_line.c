95e0e05c2e5eff586d9da5b9671c1948e092e55b
Tamires Braga <tamirescamposbraga@gmail.com>
2024-06-11 15:25:59 +0200

    arregle un segv

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,34 @@
 #include "get_next_line.h"
 
+
+char    *read_function(int fd, char *storage)
+{
+    char *buffer;
+    ssize_t chars_read;
+    char *aux; 
+
+    buffer = ft_calloc (BUFFER_SIZE + 1, sizeof(char));
+    if (!buffer)
+        return (0);    
+    while (!ft_strchr(storage, '\n'))
+    {
+        chars_read = read (fd, buffer, BUFFER_SIZE);
+        if (chars_read == -1)
+        {
+            free(storage);
+            free(buffer);
+            return (NULL);
+        }
+        if (chars_read == 0)
+            break;
+        aux = storage;
+        storage = ft_strjoin(storage, buffer); // Proteção para o malloc do strjoin
+        free(aux);
+    }
+    free(buffer);
+    return (storage);
+}
+
 static  char *catch_line(char *str)
 {
     int     i;
@@ -10,7 +39,9 @@ static  char *catch_line(char *str)
         return (NULL);
     while (str[i] != '\n' && str[i] != '\0')
         i++;
-    line = ft_calloc(sizeof(char), i + 2);
+    if (str[i] == '\n')
+      i++;
+    line = ft_calloc(sizeof(char), i + 1);
     if (!line)
         return (NULL);
     i = 0;
@@ -25,97 +56,44 @@ static  char *catch_line(char *str)
     return (line);
 }
 
-static char    *get_line_from_buffer(char *buffer, char *line)
-{ 
-    char    *aux;
-    char    *post_nl;
+char    *reload_storage(char *storage)
+{
     int     len;
-    char    *free_catch;
+    char    *post_nl;
 
-    free_catch = catch_line(buffer);
-    post_nl = ft_strchr(buffer, '\n');
-    if(post_nl == NULL)
+    post_nl = ft_strchr(storage, '\n');
+    if (!post_nl)
     {
-        aux = line;
-        line = ft_strjoin(line, buffer);
-        free(aux);
-        if (!line)
-            return(NULL);
+        free(storage);
+        return (NULL);
     }
-    else
+    len = ft_strlen(post_nl + 1);
+    if (len == 0)
     {
-        len = ft_strlen(post_nl + 1);
-        aux = line;
-        line = ft_strjoin(line, free_catch);
-        free(free_catch);
-        free(aux);
-        if (!line)
-            return(NULL);
-        ft_memmove(buffer, post_nl + 1, len);
-        buffer[len] = '\0';
+        free(storage);
+        return (NULL);
     }
-    return(line);
-
+    ft_memmove(&storage[0], post_nl + 1, len);
+    storage[len + 1] = '\0';
+    return (storage);
 }
-static int    initialize(char **buffer, char **line)
-{
-    if (*buffer == NULL)
-    {
-        *buffer = ft_calloc(sizeof(char) ,(BUFFER_SIZE + 1));
-        if (!*buffer)
-            return(0);
-    }
-    *line = ft_calloc(sizeof(char), 1);
-    if (!*line)
-    {
-        free(buffer);
-        return(0);
-    }
-    *line[0] = '\0';
-    return(1);
-}
-char *get_next_line(int fd)
-{
-    static char *buffer;
-    char *line;
-    int  chars_read;
 
-    if (fd < 0 || BUFFER_SIZE < 1)
+
+char    *get_next_line(int fd)
+{
+    static char *storage;
+    char    *line;
+
+    storage = read_function(fd, storage);//Primeiro
+    if (!storage)
         return(NULL);
-    if (read(fd, buffer, 0) < 0)
-        return(NULL);
-    if (!initialize(&buffer, &line))
-        return(NULL);
-    if (buffer != NULL && buffer[0] != '\0')
+    line = catch_line(storage);
+    if (!line)
     {
-        line = get_line_from_buffer(buffer, line);
-        if(!line){
-            free(buffer);
-            buffer = NULL;
-            return(NULL);
-        }
-        if (ft_strchr(line, '\n'))
-            return(line);
+        free(storage);
+        storage = NULL;
+        return (NULL);
     }
-    while((chars_read = read(fd, buffer, BUFFER_SIZE)) > 0)
-    {
-        buffer[chars_read] = '\0';
-        line = get_line_from_buffer(buffer, line);
-        if (!line)
-        {
-            free(buffer);
-            buffer = NULL;
-            return(NULL);
-        }
-        if (ft_strchr(line, '\n'))
-            return(line);
-    }
-    if (line[0] == '\0')
-    {
-        free(line);
-        return(NULL);
-    }
-    free(buffer);
-    buffer = NULL;
+    storage = reload_storage(storage);
     return (line);
 }
