e7d4a792ded6ff77c81a0ce005869a716d7c82ab
Tamires Campos Braga Rios <tacampos@c1r12s6.42madrid.com>
2024-06-14 14:47:08 +0200

    listo con bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,20 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+int		ft_strlen(const char *s);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
